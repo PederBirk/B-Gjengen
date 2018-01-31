d63fc29c5b4bbe191d812bb331559d7561392b2a
PederBirk <peder.birk@gmail.com>
2018-01-31 14:42:58 +0100

    La til split
    split tar et bile av en ligning og gir ut en liste med individuelle tegn
    som egne bilder

---
+++ OpenCV-test/split.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np 
+
+img = cv2.pyrDown(cv2.imread('pic.jpg', cv2.IMREAD_UNCHANGED))
+ 
+# threshold image
+ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 180, 255, cv2.THRESH_BINARY)
+
+# find contours and get the external one
+image, contours, hier = cv2.findContours(threshed_img, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+ 
+characters = []
+ 
+for c in contours:
+	# get the bounding rect
+	x, y, w, h = cv2.boundingRect(c)
+	# split original image into new images containing each character
+	if(h+w>25 and h+w<200): #disregard tiny and huge components
+		characters.append(threshed_img[y:y+h,x:x+w]) #extract the relevant areas of the image
+
+for char in characters:
+	cv2.imshow("char", char)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
