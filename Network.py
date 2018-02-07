890ead8007ccb55b5d9bcac80df283c977920935
PederBirk <peder.birk@gmail.com>
2018-02-07 21:00:13 +0100

    Half Working NN

--- Network.py
+++ Network.py
@@ -13,23 +13,29 @@ class Network():
 			
 		for x, y in zip(nodesInLayer[:-1], nodesInLayer[1:]):
 			self.weights.append(np.random.randn(y,x)) #init weights with random numbers
-			
-	def sigmoid(z):
-		return 1.0/(1.0+np.exp(-z))
 		
 	def feedForward(self, input): #Push data trough network, get prediction
-		return None
+		h = self.numLayers - 2
+		a=[sigmoid(np.dot(self.weights,input)-self.biases[0])]
+	
+		for i in range(1,h+1):
+			a.append(sigmoid(np.dot(self.weights[i],a[i-1])-self.biases[i]))
+			return a[-1]
 		
 	#Main function for processing traning data and updating network using SGD
-	def train(self, trainigData, epochs, batchSize, learningRate, testData = None):
-		n= len(trainigData)
-		for j in xrange(epochs):
-			random.shuffle(trainigData) 
+	def train(self, trainingData, epochs, batchSize, learningRate, testData = None):
+		if testData : nTest = len(testData)
+		n= len(trainingData)
+		for j in range(epochs):
+			random.shuffle(trainingData) 
 			batches = []
-			for k in xrange(0, n, batchSize):
-				batches.append[trainigData[k:k+batchSize]] #Partiton training data into batches
+			batches = [ trainingData[k:k + batchSize]
+			                               for k in range(0, n, batchSize)] #Partiton training data into batches
 			for batch in batches:
 				self.updateBatch(batch, learningRate) #Run update batch for each batch
+				
+			print("epoch: ", j, " over")
+			print("correct: ", self.evaluate(testData), "/", nTest)
 		
 	#Adjusts weights and biases in network for one batch
 	def updateBatch(self, batch, learningRate):
@@ -37,13 +43,12 @@ class Network():
 		nablaW = [np.zeros(w.shape) for w in self.weights]
 		
 		for input, expectedOutput in batch:
-			deltaNablaB, deltaNablaW = backProp(input, expectedOutput) #Get gradient for each dataset in batch
+			deltaNablaB, deltaNablaW = self.backProp(input, expectedOutput) #Get gradient for each dataset in batch
 			nablaB = [nb + dnb for nb, dnb in zip(nablaB, deltaNablaB)] #Sum the gradients
 			nablaW = [nw + dnw for nw, dnw in zip(nablaW, deltaNablaW)] # ditto
 			
-		for layer in xrange(len(self.weights)):
-			avgGradient = nablaW[layer]/len(batch) 
-			self.weights[layer] = self.weights[learningRate] - avgGradient*learningRate #Apply average gradient * learning rate
+		self.weights = [w-(learningRate/len(batch))*nw for w, nw in zip(self.weights, nablaW)] #Apply average gradient * learning rate
+		self.biases = [b-(learningRate/len(batch))*nb for b, nb in zip(self.biases, nablaB)]
 		
 	#Calculates gradient of cost function
 	def backProp(self, input, expectedOutput):
@@ -62,27 +67,29 @@ class Network():
 			
 		delta = self.costDerivative(activations[-1], expectedOutput) * sigmoidPrime(zs[-1]) #Find derivative of cost function
 		nablaB[-1] = delta
-		nablaW[-1] = np.dot(delta, activations[-2].traspose())
+		nablaW[-1] = np.dot(delta, activations[-2].transpose())
 		
-		for layer in xrange(2, self.numLayers): #starting at 2nd to last layer, work backwards and find gradient for biases and weights
+		for layer in range(2, self.numLayers): #starting at 2nd to last layer, work backwards and find gradient for biases and weights
 			z = zs[-layer]
 			sp = sigmoidPrime(z)
-			delta = np.dot(self.weights[-layer+1].traspose(), delta) * sp
+			delta = np.dot(self.weights[-layer+1].transpose(), delta) * sp
 			nablaB[-layer] = delta
-			nablaW[-layer] = np.dot(delta, activations[-layer-1].traspose())
+			nablaW[-layer] = np.dot(delta, activations[-layer-1].transpose())
 		
 		return (nablaB, nablaW)
 		
 	#Runs test data and returns % of data correctly indentified
 	def evaluate(self, testData):
-		return None
+		testResults = [(np.argmax(self.feedForward(x)),y) for (x,y) in testData]
+		return sum(int(x==y) for (x,y) in testResults)
 		
 	#Returns partial derivatives
 	def costDerivative(self, output, expectedOutput):
 		return (output-expectedOutput)
-		
-	def sigmoidPrime(z):
-		return sigmoid(z)*(1-sigmoid(z))
 	
 	
-		
+def sigmoid(z):
+	return 1.0/(1.0+np.exp(-z))
+	
+def sigmoidPrime(z):
+	return sigmoid(z)*(1-sigmoid(z))
--- testNetwork.py
+++ testNetwork.py
@@ -1,6 +1,6 @@
 import Network
 import mnist_loader
 
-net = Network.Network([784,30,10])
+net = Network.Network([784,100,10])
 trainingData, validationData, testData = mnist_loader.load_data_wrapper()
-net.train(trainingData, 30 ,10, 3.0)
+net.train(trainingData, 30 ,10, 3.0, testData = testData)
