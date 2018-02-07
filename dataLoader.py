215e5183bd76714d4fb02db3130cb2c280fd69fb
Tobias Torben <t_torben@hotmail.com>
2018-02-07 21:32:55 +0100

    Finished data loader. Added gitignore file

--- dataLoader.py
+++ dataLoader.py
@@ -1,31 +1,39 @@
 import cv2
 import numpy as np
 from os import listdir
-import os.path
 
+#Read data into list of tuples (imagePixels, expectedOutput)
+#imagePixels is a binary column vector, expectedOutput is a unit column vector
+#Retruns two lists: trainingData and testData
+def loadData(symbols, nTrainingData, nTestData, pathToData):
 
-symbols = ['0','1','2','3','4','5','6','7','8','9']
-pathToData = '../extracted_images/'
+    trainingData = []
+    testData = []
+    
+    for symbol in symbols:
+        trainingCounter = 0
+        testCounter = 0
+        expectedOutput = vectorize(symbol,symbols)
+        path = pathToData + symbol
+        for file in listdir(path):
+            if trainingCounter < nTrainingData/len(symbols):
+                trainingData.append((readImage(pathToData + symbol + '/'  + file), expectedOutput))
+                trainingCounter += 1
+            elif testCounter < nTestData/len(symbols):
+                testData.append((readImage(pathToData + symbol + '/'  + file), expectedOutput))
+                testCounter += 1
+            else:
+                break
+    return (trainingData, testData)
 
-data = []
-
-files = listdir(pathToData)
-
-for symbol in symbols:
-    expectedOutput = vectorize(symbol,symbols)
-    path = pathToData + symbol
-    for file in listdir(path):
-        data.append(zip(readImage(file),expectedOutput))
-
-
+#Create unit vector representation of symbol. Indexing is determined by 'symbols' list
 def vectorize(symbol, symbols):
     output = np.zeros((len(symbols),1))
     output[symbols.index(symbol)] = 1
     return output
 
+#Read a single image into a binary column vector
 def readImage(path):
-    img = cv2.pyrDown(cv2.imread(path, cv2.IMREAD_UNCHANGED))
-    ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 180, 255, cv2.THRESH_BINARY)
-    img.reshape(img.size,1)
-    return img
- 
+    img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+    tresh,binImg = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    return binImg.flatten()
