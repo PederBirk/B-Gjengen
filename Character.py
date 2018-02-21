4afb62500d75f8a93574349fe8c9429cef45dcb1
Tobias Torben <t_torben@hotmail.com>
2018-02-21 15:00:32 +0100

    Further work on imageprocessor

--- Character.py
+++ Character.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 
 class Character:
-
+	
 	symbol = None #The symbol the neural network has identified
+	
 	   
 	def __init__(self,image,xPos,yPos,width,height):
 		self.image = image #Image matrix to input to Neural Network
@@ -11,6 +12,7 @@ class Character:
 		self.yPos = yPos #y position of top left corner of bounding box in image
 		self.width = width #width of bounding box
 		self.height = height #height of bounding box
+		
 		
 	def show(self):
 		cv2.imshow("Character", self.image)
--- imageProcessor.py
+++ imageProcessor.py
@@ -1,13 +1,13 @@
 import cv2
 import Character as ch
-import numpy as np 
+import numpy as np
 
-def getBinaryImg(path, tresh_lower = 180, tresh_upper = 255):
-	img = cv2.pyrDown(cv2.imread('images/pic.jpg', cv2.IMREAD_UNCHANGED))
-	ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 180, 255, cv2.THRESH_BINARY)
-	return threshed_img
+def getImgMat(path, thresh_lower = 180, thresh_upper = 255):
+	img = cv2.pyrDown(cv2.imread(path, cv2.IMREAD_UNCHANGED))
+	threshed_image = cv2.adaptiveThreshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,41,6)
+	return threshed_image
 
-def extractCharacters(binImg):
+def extractCharacters(binImg, targetResolution = (45,45), thresh_lower = 180, thresh_upper = 255):
 	image, contours, hier = cv2.findContours(binImg, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	characters = [] 
 	for c in contours:
@@ -15,6 +15,34 @@ def extractCharacters(binImg):
 		if (h+w>25 and h+w<200): #disregard tiny and huge components
 			img = binImg[y:y+h,x:x+w]
 			char = ch.Character(img,x,y,w,h)
+			squareImage(char)
+			resize(char,targetResolution)
+			ret, char.image = cv2.threshold(char.image, thresh_lower, thresh_upper, cv2.THRESH_BINARY)
 			characters.append(char)
 	return characters
 
+def squareImage(char):
+	w = char.width
+	h = char.height
+	if w > h:
+		diff = w-h
+		padding = int(diff/2.0)
+		dim = (padding,w)
+		paddingMat = 255*np.ones(dim)
+		char.image = np.concatenate((paddingMat,char.image,paddingMat),0)
+	elif h > w:
+		diff = h-w
+		padding = int(diff/2.0)
+		dim = (h,padding)
+		paddingMat = 255*np.ones(dim)
+		char.image = np.concatenate((paddingMat,char.image,paddingMat),1)
+		
+	size = char.image.shape
+	if size[0] > size[1]:
+		char.image = np.concatenate((char.image,255*np.ones((h,1))),1)
+	elif size[1] > size[0]:
+		char.image = np.concatenate((char.image,255*np.ones((1,w))),0)
+		
+def resize(char,targetResolution):
+	char.image = cv2.resize(char.image,targetResolution);
+	
