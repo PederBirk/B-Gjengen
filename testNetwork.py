20121c889f028ab3cfb6692beb305720a5554176
Jarl Gunnar T. Flaten <jarl.flaten@gmail.com>
2018-02-21 11:09:50 +0100

    update network for testing on cluster

--- testNetwork.py
+++ testNetwork.py
@@ -1,6 +1,8 @@
 import Network
 import mnist_loader
 
-net = Network.Network([784,100,10])
+# updated for testing on the cluster; branch cluster 
+net = Network.Network([784,342,171,57,10])
 trainingData, validationData, testData = mnist_loader.load_data_wrapper()
-net.train(trainingData, 30 ,10, 3.0, testData = testData)
+
+net.train(trainingData, 30, 100, 3.0, testData = testData)
